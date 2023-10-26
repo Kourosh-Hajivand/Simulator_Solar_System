33b895e253e54c19f87a5789a015345017329026
kourosh-H <kourosh.hajivand0@gmail.com>
2023-10-26 17:00:19 +0330

    🎨 Test

--- main.js
+++ main.js
@@ -3,22 +3,19 @@ import * as THREE from "three";
 const scene = new THREE.Scene();
 // The Shape
 const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshStandardMaterial({
-  color: "#00ff83",
-});
-const Mesh = new THREE.Mesh(geometry, material);
-scene.add(Mesh);
-
+const material = new THREE.MeshBasicMaterial({ color: "#00ff83" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 // Light
-const Light = new THREE.PointLight(0xffffff, 1, 100);
-Light.position.set(0, 10, 10);
-scene.add(Light);
-
-const camara = new THREE.PerspectiveCamera(45, 800 / 600);
-camara.position.z = 20;
-scene.add(camara);
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+// Camera
+const camera = new THREE.PerspectiveCamera(30, 800 / 600);
+camera.position.z = 20;
+scene.add(camera);
 // Rendrer
-const canvas = document.querySelector(".webGl");
-const Rendrer = new THREE.WebGLRenderer({ canvas });
-Rendrer.setSize(800, 600);
-Rendrer.render(scene, camara);
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(800, 600);
+renderer.render(scene, camera);
