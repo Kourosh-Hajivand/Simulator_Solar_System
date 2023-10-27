12b97507c0a0da92bcd6b5b25523f7cd0ff1ffa8
kourosh-H <kourosh.hajivand0@gmail.com>
2023-10-27 11:11:58 +0330

    ⚡️ Update

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
 import * as THREE from "three";
-// Add scene
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
 const scene = new THREE.Scene();
-// The Shape
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshBasicMaterial({ color: "#00ff83" });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-// Light
-const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(0, 10, 10);
-scene.add(light);
 // Camera
-const camera = new THREE.PerspectiveCamera(30, 800 / 600);
-camera.position.z = 20;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.set(0, 2, 10);
 scene.add(camera);
-// Rendrer
+// AxesHelper
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// BoxGeometry
+const Geometry = new THREE.BoxGeometry();
+const Material = new THREE.MeshBasicMaterial({ color: "#00ff83" });
+const box = new THREE.Mesh(Geometry, Material);
+scene.add(box);
+//  PlaneGeometry
+const PlaneGeometry = new THREE.PlaneGeometry(10, 10);
+const PlaneMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  side: THREE.DoubleSide,
+});
+// GridHepler
+const GridHepler = new THREE.GridHelper();
+scene.add(GridHepler);
+const PlaneMesh = new THREE.Mesh(PlaneGeometry, PlaneMaterial);
+PlaneMesh.rotation.x = -0.5 * Math.PI;
+scene.add(PlaneMesh);
+const animtaion = () => {
+  box.rotation.y += 0.05;
+
+  renderer.render(scene, camera);
+};
 const canvas = document.querySelector(".webgl");
+const orbitcontrol = new OrbitControls(camera, canvas);
+orbitcontrol.update();
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
-renderer.render(scene, camera);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animtaion);
