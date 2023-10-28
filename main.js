453ccab9f852d2b31ccd1015f4b85b8165a9a71a
kourosh-H <kourosh.hajivand0@gmail.com>
2023-10-28 16:57:05 +0330

    Update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
+import * as dat from "dat.gui";
 const scene = new THREE.Scene();
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -16,17 +16,21 @@ scene.add(camera);
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-// BoxGeometry
-const Geometry = new THREE.BoxGeometry();
-const Material = new THREE.MeshBasicMaterial({ color: "#00ff83" });
-const box = new THREE.Mesh(Geometry, Material);
-scene.add(box);
+// // BoxGeometry
+// const Geometry = new THREE.BoxGeometry();
+// const Material = new THREE.MeshBasicMaterial({ color: "#00ff83" });
+// const box = new THREE.Mesh(Geometry, Material);
+// scene.add(box);
 //  PlaneGeometry
-const PlaneGeometry = new THREE.PlaneGeometry(10, 10);
-const PlaneMaterial = new THREE.MeshBasicMaterial({
+const PlaneGeometry = new THREE.PlaneGeometry(40, 40);
+const PlaneMaterial = new THREE.MeshStandardMaterial({
   color: 0xffffff,
   side: THREE.DoubleSide,
 });
+const PlaneMesh = new THREE.Mesh(PlaneGeometry, PlaneMaterial);
+PlaneMesh.rotation.x = -0.5 * Math.PI;
+
+scene.add(PlaneMesh);
 // SepherGeometry
 const SepherGeometry = new THREE.SphereGeometry(2, 64, 64);
 const SepherMaterial = new THREE.MeshLambertMaterial({
@@ -34,22 +38,41 @@ const SepherMaterial = new THREE.MeshLambertMaterial({
   wireframe: false,
 });
 const SepherMesh = new THREE.Mesh(SepherGeometry, SepherMaterial);
-SepherMesh.position.x = 3;
 scene.add(SepherMesh);
 // Light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 10);
-directionalLight.position.set(0, 1, 0); // Set the light's position
+const AmbientLight = new THREE.AmbientLight(0x333333);
+scene.add(AmbientLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(-10, 10, 0);
 scene.add(directionalLight);
+const DLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+scene.add(DLightHelper);
 // GridHepler
+// const GridHepler = new THREE.GridHelper();
+// scene.add(GridHepler);
 
-const GridHepler = new THREE.GridHelper();
-scene.add(GridHepler);
-const PlaneMesh = new THREE.Mesh(PlaneGeometry, PlaneMaterial);
-PlaneMesh.rotation.x = -0.5 * Math.PI;
-scene.add(PlaneMesh);
+// Add Gui
+const gui = new dat.GUI();
+const option = {
+  SepherColor: "#ffea00",
+  speed: 0.01,
+  positionX: 0,
+  wireframe: false,
+};
+gui.addColor(option, "SepherColor").onChange((e) => {
+  SepherMesh.material.color.set(e);
+});
+gui.add(option, "speed", 0, 0.1);
+gui.add(option, "positionX").onChange((e) => {
+  SepherMesh.position.x = e;
+});
+gui.add(option, "wireframe").onChange((e) => {
+  SepherMesh.material.wireframe = e;
+});
+let step = 0;
 const animtaion = () => {
-  box.rotation.y += 0.05;
-
+  step += option.speed;
+  SepherMesh.position.y = 5 * Math.abs(Math.sin(step));
   renderer.render(scene, camera);
 };
 const canvas = document.querySelector(".webgl");
@@ -57,4 +80,5 @@ const orbitcontrol = new OrbitControls(camera, canvas);
 orbitcontrol.update();
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 renderer.setAnimationLoop(animtaion);
